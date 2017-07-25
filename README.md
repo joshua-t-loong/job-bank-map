c1bf0f9563144fbe7e3a03eb55b82e4f50f0a9e8
joshua-t-loong <joshua.t.loong@gmail.com>
2017-07-24 20:28:13 -0400

    Added a semi working search bar

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Job Bank Map
 The current Canadian Job Bank site allows people to search for job postings across the country in all different kinds of industries.
-The public version of the site allows you to search by town or by an occupation, pulling up a list of postings. 
+The public version of the site allows you to search by town or by an occupation, pulling up a list of postings.
 This is good when an individual has a very specific understanding of the type of job they want, or know exactly where they want to work.
 
-However, with the current labour market being difficult to break into, many individuals, especially young people, are just looking 
+However, with the current labour market being difficult to break into, many individuals, especially young people, are just looking
 for any kind of work that is within a good distance from their home. The best way for people to find this information is spatially.
 
-Using the beta API ([found here](http://stage.lmi-explore-imt.ca/lmiws-api/job/jbws/ct)) that I found in the current Labour Market Information site, I've built an interactive map on top of Mapbox that 
-allows people to look at the available jobs in their area. 
+Using the beta API ([found here](http://stage.lmi-explore-imt.ca/lmiws-api/job/jbws/ct)) that I found in the current Labour Market Information site, I've built an interactive map on top of Mapbox that
+allows people to look at the available jobs in their area.
 
 ## Current Features
 * Map of cities within the Job Bank database
@@ -17,5 +17,5 @@ allows people to look at the available jobs in their area.
 ## Future Features
 * Reverse geocoding to get town/city names in the popups
 * Search for town/city names
+* Search for occupations
 * A preview of the most recent jobs posted
-
--- index.js
+++ index.js
@@ -2,9 +2,70 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zaHVhLWxvb25nIiwiYSI6ImNqMm01ODQ4cTAwc28yc
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    center: [-96, 37.8],
+    center: [-96, 60],
     zoom: 3,
 });
+
+
+
+$("#search-input").keypress(function(e) {
+     if (e.which == 13) {
+          requestData($("#search-input").val());
+          return false;
+     }
+});
+
+function requestData(noc)
+{
+  map.removeLayer("points");
+  $.get("http://stage.lmi-explore-imt.ca/lmiws-api/job/jbws/ct/noc?id=" + noc, function(data){drawData(data)});
+}
+
+function drawData(dataUncompressed)
+{
+
+  var jobData = [], data = JSON.parse(dataUncompressed);
+  for(var i in data)
+  {
+    var coordinates = data[i]["x"],
+      x = Number(coordinates.substr(10)),
+      y = Number(coordinates.substr(0, 9));
+    // town = getTownName (x, y);
+    jobdata[i] = {
+      "type": "Feature",
+      "geometry": {
+          "type": "Point",
+          "coordinates": [x, y]
+        },
+        "properties": {
+            "title": data[i]["c"],
+            "icon": "marker",
+            "description": "<strong>This town has " + data[i]["c"] + " job posting(s).</strong> <p>Visit the <a href=https://www.jobbank.gc.ca/jobsearch/jobsearch?cty=" + data[i]["k"]+ "&wid=px&sort=M#results-list-content target=_blank>Job Bank site</a> for full posting information </p>"
+        }
+    }
+  }
+
+  map.addLayer({
+      "id": "points",
+      "type": "symbol",
+      "source": {
+          "type": "geojson",
+          "data": {
+              "type": "FeatureCollection",
+              "features": jobdata
+          }
+      },
+      "layout": {
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+      }
+    });
+
+  console.log("Hello");
+}
 
 map.on('load', function () {
   var xhr = new XMLHttpRequest();
@@ -17,10 +78,9 @@ map.on('load', function () {
 
     for(var i in data)
     {
-
       var coordinates = data[i]["x"],
         x = Number(coordinates.substr(10)),
-        y = Number( coordinates.substr(0, 9));
+        y = Number(coordinates.substr(0, 9));
       // town = getTownName (x, y);
       jobdata[i] = {
         "type": "Feature",
@@ -35,6 +95,7 @@ map.on('load', function () {
           }
       }
     }
+
     map.addLayer({
         "id": "points",
         "type": "symbol",
@@ -61,43 +122,21 @@ map.on('load', function () {
   }
   xhr.open('GET','http://stage.lmi-explore-imt.ca/lmiws-api/job/jbws/ct' ,true);
   xhr.send();
-
-  map.on('click', 'points', function (e) {
-   new mapboxgl.Popup()
-       .setLngLat(e.features[0].geometry.coordinates)
-       .setHTML(e.features[0].properties.description)
-       .addTo(map);
-   });
-
-   // Change the cursor to a pointer when the mouse is over the places layer.
-   map.on('mouseenter', 'points', function () {
-       map.getCanvas().style.cursor = 'pointer';
-   });
-
-   // Change it back to a pointer when it leaves.
-   map.on('mouseleave', 'points', function () {
-       map.getCanvas().style.cursor = '';
-   });
-
-
 });
 
+map.on('click', 'points', function (e) {
+ new mapboxgl.Popup()
+     .setLngLat(e.features[0].geometry.coordinates)
+     .setHTML(e.features[0].properties.description)
+     .addTo(map);
+ });
 
-  //  function getTownName(x, y)
-  //  {
-  //    var xhr = new XMLHttpRequest();
-  //    xhr.onreadystatechange = function(){
-  //    if (xhr.readyState==4 && xhr.status==200)
-  //    {
-  //      var data = JSON.parse(xhr.responseText);
-  //      console.log(data);
-  //      var town = data["features"][0]["context"][3]["text"];
-  //      return town;
-   //
-  //    }
-  //  }
-  //    xhr.open('GET','https://api.mapbox.com/geocoding/v5/mapbox.places/'+ x + '%2C%20%09' + y + '.json?access_token=pk.eyJ1Ijoiam9zaHVhLWxvb25nIiwiYSI6ImNqMm01ODQ4cTAwc28ycW93ZGV1cWR5amIifQ.ZxuCP_hBIHqxPUon6vX7jg&proximity=' ,true);
-  //    xhr.send();
-   //
-   //
-  //  }
+ // Change the cursor to a pointer when the mouse is over the places layer.
+ map.on('mouseenter', 'points', function () {
+     map.getCanvas().style.cursor = 'pointer';
+ });
+
+ // Change it back to a pointer when it leaves.
+ map.on('mouseleave', 'points', function () {
+     map.getCanvas().style.cursor = '';
+ });
